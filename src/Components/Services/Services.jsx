10b6d23f1e89d3373ea95c0ce2aa5927bd310245
uae <aniekaudo@yahoo.com>
2022-07-18 20:06:34 +0100

    :zap: Experience section
    Created experience component

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -6,7 +6,7 @@ import Glasses from "../../img/glasses.png";
 import Humble from "../../img/humble.png";
 import { themeContext } from "../../Context";
 import { motion } from "framer-motion";
-import Resume from './resume.pdf';
+// import Resume from './resume.pdf';
 
 const Services = () => {
   // context
@@ -20,66 +20,71 @@ const Services = () => {
   };
 
   return (
-    <div className="services" id="services">
+    <div className='services' id='services'>
       {/* left side */}
-      <div className="awesome">
+      <div className='awesome'>
         {/* dark mode */}
-        <span style={{ color: darkMode ? "white" : "" }}>My Awesome</span>
+        <span style={{ color: darkMode ? 'white' : '' }}>My Awesome</span>
         <span>services</span>
         <spane>
-          Lorem ispum is simpley dummy text of printing of printing Lorem
+          Custom software development, web apllication development, web design
           <br />
-          ispum is simpley dummy text of printing
+          mobile application development, quality assurance
         </spane>
-        <a href={Resume} download>
-          <button className="button s-button">Download CV</button>
+        {/* <a href={Resume} download></a> */}
+
+        <a
+          href='https://docs.google.com/document/d/e/2PACX-1vTq31MWjNhonbx3Xk2Ds7A0e0VW-6uWZQ16uuMJSCoQaE8RE9mdagYJaGVcNPHkwNrc7eDwYezc4EAM/pub'
+          download
+        >
+          <button className='button s-button'> Get my resume</button>
         </a>
-        <div className="blur s-blur1" style={{ background: "#ABF1FF94" }}></div>
+        <div className='blur s-blur1' style={{ background: '#ABF1FF94' }}></div>
       </div>
       {/* right */}
-      <div className="cards">
+      <div className='cards'>
         {/* first card */}
         <motion.div
-          initial={{ left: "25rem" }}
-          whileInView={{ left: "14rem" }}
+          initial={{ left: '25rem' }}
+          whileInView={{ left: '14rem' }}
           transition={transition}
         >
           <Card
             emoji={HeartEmoji}
-            heading={"Design"}
-            detail={"Figma, Sketch, Photoshop, Adobe Illustrator, Adobe xd"}
+            heading={'Design'}
+            detail={'Figma, Sketch, Photoshop, Adobe Illustrator, Adobe xd'}
           />
         </motion.div>
         {/* second card */}
         <motion.div
-          initial={{ left: "-11rem", top: "12rem" }}
-          whileInView={{ left: "-4rem" }}
+          initial={{ left: '-11rem', top: '12rem' }}
+          whileInView={{ left: '-4rem' }}
           transition={transition}
         >
           <Card
             emoji={Glasses}
-            heading={"Developer"}
-            detail={"Html, Css, JavaScript, React, Nodejs, Express"}
+            heading={'Developer'}
+            detail={'Html, Css, JavaScript, React, Nodejs, Express'}
           />
         </motion.div>
         {/* 3rd */}
         <motion.div
-          initial={{ top: "19rem", left: "25rem" }}
-          whileInView={{ left: "12rem" }}
+          initial={{ top: '19rem', left: '25rem' }}
+          whileInView={{ left: '12rem' }}
           transition={transition}
         >
           <Card
             emoji={Humble}
-            heading={"UI/UX"}
+            heading={'UI/UX'}
             detail={
-              "Lorem ispum dummy text are usually use in section where we need some random text"
+              'Lorem ispum dummy text are usually use in section where we need some random text'
             }
-            color="rgba(252, 166, 31, 0.45)"
+            color='rgba(252, 166, 31, 0.45)'
           />
         </motion.div>
         <div
-          className="blur s-blur2"
-          style={{ background: "var(--purple)" }}
+          className='blur s-blur2'
+          style={{ background: 'var(--purple)' }}
         ></div>
       </div>
     </div>
