43c2bb079c9710ad4e32dd1e17a35e5b3fd7bfb1
uae <aniekaudo@yahoo.com>
2022-07-23 13:33:56 +0100

    :sparkles: Footer
    Created footer componnet

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Footer.css";
-import Wave from "../../img/wave.png";
+import secondwave2 from "../../img/secondwave2.png";
 import Insta from "@iconscout/react-unicons/icons/uil-instagram";
 import Facebook from "@iconscout/react-unicons/icons/uil-facebook";
 import Gitub from "@iconscout/react-unicons/icons/uil-github";
@@ -8,7 +8,7 @@ import Gitub from "@iconscout/react-unicons/icons/uil-github";
 const Footer = () => {
   return (
     <div className="footer">
-      <img src={Wave} alt="" style={{ width: "100%" }} />
+      <img src={secondwave2} alt="" style={{ width: "100%" }} />
       <div className="f-content">
         <span>uaniekan@gmail.com</span>
         <div className="f-icons">
