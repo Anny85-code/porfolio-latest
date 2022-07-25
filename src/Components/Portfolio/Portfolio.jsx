fcd5022c654899651783af18f779d91b1ce366f4
uae <aniekaudo@yahoo.com>
2022-07-25 14:50:56 +0100

    :art: Portfoilio
    Updated portfolio images

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -2,10 +2,10 @@ import React, { useContext } from "react";
 import "./Portfolio.css";
 import { Swiper, SwiperSlide } from "swiper/react"
 import "swiper/css";
-import Sidebar from "../../img/sidebar.png";
-import Ecommerce from "../../img/ecommerce.png";
-import HOC from "../../img/hoc.png";
-import MusicApp from "../../img/musicapp.png";
+import budget from "../../img/budget.png";
+import dravel from "../../img/dravel.png";
+import food from "../../img/food.png";
+import seaport from "../../img/seaport.png";
 import { themeContext } from "../../Context";
 const Portfolio = () => {
   const theme = useContext(themeContext);
@@ -24,20 +24,20 @@ const Portfolio = () => {
         className='portfolio-slider'
       >
         <SwiperSlide>
-          <img src={Sidebar} alt='' />
+          <img src={budget} alt='' />
           <div className='button-img'>
             <button className='button-img-inner'>see more</button>
             <button className='button-img-inner'>see live</button>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <img src={Ecommerce} alt='' />
+          <img src={dravel} alt='' />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={MusicApp} alt='' />
+          <img src={food} alt='' />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={HOC} alt='' />
+          <img src={seaport} alt='' />
         </SwiperSlide>
       </Swiper>
     </div>
