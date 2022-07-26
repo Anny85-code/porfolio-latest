caf2471b5956ca328eb671262bfc8e9ec98a6740
uae <aniekaudo@yahoo.com>
2022-07-26 16:11:04 +0100

    :rotating_light: Linters
    Fix linters in portfofolio page

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,18 +1,18 @@
-import React, { useContext } from "react";
-import "./Portfolio.css";
-import { Swiper, SwiperSlide } from "swiper/react"
-import "swiper/css";
-import budget from "../../img/budget.png";
-import dravel from "../../img/dravel.png";
-import food from "../../img/food.png";
-import seaport from "../../img/seaport.png";
-import { themeContext } from "../../Context";
+import React, { useContext } from 'react';
+import './Portfolio.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import budget from '../../img/budget.png';
+import dravel from '../../img/dravel.png';
+import food from '../../img/food.png';
+import seaport from '../../img/seaport.png';
+import { themeContext } from '../../Context';
 
 const Portfolio = () => {
   const theme = useContext(themeContext);
-  const darkMode = theme.state.darkMode;
+  const { darkMode } = theme.state;
   return (
-    <div className='portfolio' id='portfolio'>
+    <div className="portfolio" id="portfolio">
       {/* heading */}
       <span style={{ color: darkMode ? 'white' : '' }}>Recent Projects</span>
       <span>Portfolio</span>
@@ -21,43 +21,43 @@ const Portfolio = () => {
       <Swiper
         spaceBetween={30}
         slidesPerView={3}
-        grabCursor={true}
-        className='portfolio-slider'
+        grabCursor
+        className="portfolio-slider"
       >
         <SwiperSlide>
           <a
-            href='https://github.com/Anny85-code/budget-app'
-            target='_blank'
-            rel='noopener noreferrer'
+            href="https://github.com/Anny85-code/budget-app"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <img src={budget} alt='' />
+            <img src={budget} alt=" " />
           </a>
         </SwiperSlide>
         <SwiperSlide>
           <a
-            href='https://github.com/Anny85-code/dravel_app'
-            target='_blank'
-            rel='noopener noreferrer'
+            href="https://github.com/Anny85-code/dravel_app"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <img src={dravel} alt='' />
+            <img src={dravel} alt="" />
           </a>
         </SwiperSlide>
         <SwiperSlide>
           <a
-            href='https://github.com/Anny85-code/food-recipe'
-            target='_blank'
-            rel='noopener noreferrer'
+            href="https://github.com/Anny85-code/food-recipe"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <img src={food} alt='' />
+            <img src={food} alt="" />
           </a>
         </SwiperSlide>
         <SwiperSlide>
           <a
-            href='https://github.com/Anny85-code/seafoodmenu'
-            target='_blank'
-            rel='noopener noreferrer'
+            href="https://github.com/Anny85-code/seafoodmenu"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <img src={seaport} alt='' />
+            <img src={seaport} alt="" />
           </a>
         </SwiperSlide>
       </Swiper>
