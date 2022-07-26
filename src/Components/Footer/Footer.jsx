c6ab7365deaa24b556f4ee3e388401233970d0be
uae <aniekaudo@yahoo.com>
2022-07-26 21:55:48 +0100

    :art: Footer
    Update footer page

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,28 +1,29 @@
-import React from "react";
-import "./Footer.css";
-import secondwave2 from "../../img/secondwave2.png";
-import Insta from "@iconscout/react-unicons/icons/uil-instagram";
-import Facebook from "@iconscout/react-unicons/icons/uil-facebook";
-import Gitub from "@iconscout/react-unicons/icons/uil-github";
+import React from 'react';
+import './Footer.css';
+import Insta from '@iconscout/react-unicons/icons/uil-instagram';
+import Facebook from '@iconscout/react-unicons/icons/uil-facebook';
+import Gitub from '@iconscout/react-unicons/icons/uil-github';
+import secondwave2 from '../../img/secondwave2.png';
 
 const Footer = () => {
-
   const Foter = () => new Date().getFullYear();
 
   return (
     <div className="footer">
-      <img src={secondwave2} alt="" style={{ width: "100%" }} />
+      <img src={secondwave2} alt="" style={{ width: '100%' }} />
       <div className="f-content">
         <span>uaniekan@gmail.com</span>
         <div className="f-icons">
-          <Insta color="white" size={"3rem"} />
-          <Facebook color="white" size={"3rem"} />
-          <Gitub color="white" size={"3rem"} />
+          <Insta color="white" size="3rem" />
+          <Facebook color="white" size="3rem" />
+          <Gitub color="white" size="3rem" />
         </div>
-        
-        <span className="footer-co"> ©
-        {Foter()}
-        &nbsp;Anny Udo portfolio website</span>
+        <span className="footer-co">
+          ©
+          {Foter()}
+          &nbsp;
+          Anny Udo portfolio website
+        </span>
       </div>
     </div>
   );
