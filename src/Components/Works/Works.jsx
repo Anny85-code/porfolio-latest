d56ed4c14b485214330446570246391f49af2c0a
uae <aniekaudo@yahoo.com>
2022-07-26 22:20:38 +0100

    :fire: Works
    Update works component

--- src/Components/Works/Works.jsx
+++ src/Components/Works/Works.jsx
@@ -1,24 +1,25 @@
-import React, { useContext } from "react";
-import "./Works.css";
-import javascript from "../../img/javascript.png";
-import react from "../../img/react.png";
-import postgres from "../../img/postgres.png";
-import rubyonrails from "../../img/rubyonrails.png";
-import bootstrap from "../../img/bootstrap.png";
-import { themeContext } from "../../Context";
-import { motion } from "framer-motion";
+import React, { useContext } from 'react';
+import { motion } from 'framer-motion';
 import {Link} from 'react-scroll'
+import { themeContext } from '../../Context';
+import javascript from '../../img/javascript.png';
+import react from '../../img/react.png';
+import postgres from '../../img/postgres.png';
+import rubyonrails from '../../img/rubyonrails.png';
+import bootstrap from '../../img/bootstrap.png';
+import './Works.css';
+
 const Works = () => {
   // context
   const theme = useContext(themeContext);
-  const darkMode = theme.state.darkMode;
+  const { darkMode } = theme.state;
 
   // transition
   return (
-    <div className='works' id='works'>
+    <div className="works" id="works">
       {/* left side */}
-      <div className='w-left'>
-        <div className='awesome'>
+      <div className="w-left">
+        <div className="awesome">
           {/* dark Mode */}
           <span style={{ color: darkMode ? 'white' : '' }}>Technologies</span>
           <span>Stacks</span>
@@ -31,44 +32,41 @@ const Works = () => {
             <br />
             during the planning phase of projects
           </spane>
-          <Link to='contact' smooth={true} spy={true}>
-            <button className='button s-button'>Hire Me</button>
+          <Link to="contact" smooth spy>
+            <button type="button" className="button s-button">Hire Me</button>
           </Link>
-          <div
-            className='blur s-blur1'
-            style={{ background: '#ABF1FF94' }}
-          ></div>
+          <div className="blur s-blur1" style={{ background: '#ABF1FF94' }} />
         </div>
 
         {/* right side */}
       </div>
-      <div className='w-right'>
+      <div className="w-right">
         <motion.div
           initial={{ rotate: 45 }}
           whileInView={{ rotate: 0 }}
           viewport={{ margin: '-40px' }}
           transition={{ duration: 3.5, type: 'spring' }}
-          className='w-mainCircle'
+          className="w-mainCircle"
         >
-          <div className='w-secCircle'>
-            <img src={javascript} alt='' />
+          <div className="w-secCircle">
+            <img src={javascript} alt="" />
           </div>
-          <div className='w-secCircle'>
-            <img src={react} alt='' />
+          <div className="w-secCircle">
+            <img src={react} alt="" />
           </div>
-          <div className='w-secCircle'>
-            <img src={postgres} alt='' />
-          </div>{' '}
-          <div className='w-secCircle'>
-            <img src={rubyonrails} alt='' />
+          <div className="w-secCircle">
+            <img src={postgres} alt="" />
           </div>
-          <div className='w-secCircle'>
-            <img src={bootstrap} alt='' />
+          <div className="w-secCircle">
+            <img src={rubyonrails} alt="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={bootstrap} alt="" />
           </div>
         </motion.div>
         {/* background Circles */}
-        <div className='w-backCircle blueCircle'></div>
-        <div className='w-backCircle yellowCircle'></div>
+        <div className="w-backCircle blueCircle" />
+        <div className="w-backCircle yellowCircle" />
       </div>
     </div>
   );
