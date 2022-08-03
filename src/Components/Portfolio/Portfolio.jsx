97c4e8a8e516c69303bad59c8fd32ebfad01aceb
uae <aniekaudo@yahoo.com>
2022-08-03 21:49:49 +0530

    :bug: Portfolio
    Fix single quote bug

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -13,7 +13,7 @@ const Portfolio = () => {
   const theme = useContext(themeContext);
   const { darkMode } = theme.state;
   return (
-    <div className='portfolio' id='portfolio'>
+    <div className="portfolio' id='portfolio">
       {/* heading */}
       <span style={{ color: darkMode ? 'white' : '' }}>Recent Projects</span>
       <span>Portfolio</span>
@@ -23,51 +23,51 @@ const Portfolio = () => {
         spaceBetween={30}
         slidesPerView={3}
         grabCursor
-        className='portfolio-slider'
+        className="portfolio-slider"
       >
         <SwiperSlide>
           <a
-            href='https://github.com/Anny85-code/optimism'
-            target='_blank'
-            rel='noopener noreferrer'
+            href="https://github.com/Anny85-code/optimism"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <img src={foofforallport1} alt=' ' />
+            <img src={foofforallport1} alt={foofforallport1} />
           </a>
         </SwiperSlide>
         <SwiperSlide>
           <a
-            href='https://github.com/Anny85-code/budget-app'
-            target='_blank'
-            rel='noopener noreferrer'
+            href="https://github.com/Anny85-code/budget-app"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <img src={budget} alt=' ' />
+            <img src={budget} alt={budget} />
           </a>
         </SwiperSlide>
         <SwiperSlide>
           <a
-            href='https://github.com/Anny85-code/dravel_app'
-            target='_blank'
-            rel='noopener noreferrer'
+            href="https://github.com/Anny85-code/dravel_app"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <img src={dravel} alt='' />
+            <img src={dravel} alt="" />
           </a>
         </SwiperSlide>
         <SwiperSlide>
           <a
-            href='https://github.com/Anny85-code/food-recipe'
-            target='_blank'
-            rel='noopener noreferrer'
+            href="https://github.com/Anny85-code/food-recipe"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <img src={food} alt='' />
+            <img src={food} alt="" />
           </a>
         </SwiperSlide>
         <SwiperSlide>
           <a
-            href='https://github.com/Anny85-code/seafoodmenu'
-            target='_blank'
-            rel='noopener noreferrer'
+            href="https://github.com/Anny85-code/seafoodmenu"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <img src={seaport} alt='' />
+            <img src={seaport} alt="" />
           </a>
         </SwiperSlide>
       </Swiper>
