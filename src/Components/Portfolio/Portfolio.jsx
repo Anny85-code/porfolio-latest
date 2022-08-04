e9e9cf6776dca5a7508cb9ac42d2cc55f1ebfd54
uae <aniekaudo@yahoo.com>
2022-08-04 13:42:53 +0100

    :sparkles: Porfolio
    Implement hover on sloder

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,5 +1,8 @@
 import React, { useContext } from 'react';
+import { AiFillEye, AiFillGithub } from 'react-icons/ai';
+import { motion } from 'framer-motion';
 import './Portfolio.css';
+import './Portfolio.scss';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import budget from '../../img/budget.png';
@@ -13,9 +16,41 @@ import { themeContext } from '../../Context';
 const Portfolio = () => {
   const theme = useContext(themeContext);
   const { darkMode } = theme.state;
+
+  const works = [{
+    name: 'footforall',
+    imgUrl: foofforallport1,
+    projectSource: 'https://github.com/Anny85-code/optimism',
+    projectLiveLink: 'https://optimismfoodforall.herokuapp.com/',
+  }, {
+    name: 'budget',
+    imgUrl: budget,
+    projectSource: 'https://github.com/Anny85-code/budget-app',
+    projectLiveLink: 'https://salty-stream-24790.herokuapp.com/',
+  }, {
+    name: 'dravel',
+    imgUrl: dravel,
+    projectSource: 'https://github.com/Anny85-code/dravel_app',
+    projectLiveLink: 'https://dravel-app.herokuapp.com/',
+  }, {
+    name: 'food',
+    imgUrl: food,
+    projectSource: 'https://github.com/Anny85-code/food-recipe',
+    projectLiveLink: 'https://food-recipe-rails.herokuapp.com/',
+  }, {
+    name: 'seaport',
+    imgUrl: seaport,
+    projectSource: 'https://github.com/Anny85-code/seafoodmenu',
+    projectLiveLink: 'https://anny85-code.github.io/seafoodmenu/dist/',
+  }, {
+    name: 'covid19',
+    imgUrl: covid19port,
+    projectSource: 'https://github.com/Anny85-code/covid19-tracker',
+    projectLiveLink: 'https://covid19-dataapp.herokuapp.com/',
+  }];
+
   return (
     <div className="portfolio" id="portfolio">
-      {/* heading */}
       <span style={{ color: darkMode ? 'white' : '' }}>Recent Projects</span>
       <span>Portfolio</span>
 
@@ -26,60 +61,44 @@ const Portfolio = () => {
         grabCursor
         className="portfolio-slider"
       >
-        <SwiperSlide>
-          <a
-            href="https://github.com/Anny85-code/optimism"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={foofforallport1} alt={foofforallport1} />
-          </a>
-        </SwiperSlide>
-        <SwiperSlide>
-          <a
-            href="https://github.com/Anny85-code/budget-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={budget} alt={budget} />
-          </a>
-        </SwiperSlide>
-        <SwiperSlide>
-          <a
-            href="https://github.com/Anny85-code/dravel_app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={dravel} alt="" />
-          </a>
-        </SwiperSlide>
-        <SwiperSlide>
-          <a
-            href="https://github.com/Anny85-code/food-recipe"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={food} alt="" />
-          </a>
-        </SwiperSlide>
-        <SwiperSlide>
-          <a
-            href="https://github.com/Anny85-code/seafoodmenu"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={seaport} alt="" />
-          </a>
-        </SwiperSlide>
-        <SwiperSlide>
-          <a
-            href="https://github.com/Anny85-code/covid19-tracker"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={covid19port} alt="" />
-          </a>
-        </SwiperSlide>
+        {works.map((work) => (
+          <div key={work.name}>
+            <SwiperSlide>
+              <img src={work.imgUrl} alt={work.name} />
+
+              <motion.div
+                whileHover={{ opacity: [0, 1] }}
+                transition={{
+                  duration: 0.25,
+                  ease: 'easeInOut',
+                  staggerChildren: 0.5,
+                }}
+                className="app__work-hover app__flex"
+              >
+                <a href={work.projectLiveLink} target="_blank" rel="noreferrer">
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.9] }}
+                    transition={{ duration: 0.25 }}
+                    className="app__flex"
+                  >
+                    <AiFillEye />
+                  </motion.div>
+                </a>
+                <a href={work.projectSource} target="_blank" rel="noreferrer">
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.9] }}
+                    transition={{ duration: 0.25 }}
+                    className="app__flex"
+                  >
+                    <AiFillGithub />
+                  </motion.div>
+                </a>
+              </motion.div>
+            </SwiperSlide>
+          </div>
+        ))}
       </Swiper>
     </div>
   );
